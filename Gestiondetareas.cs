684105ba5fee653dd1e2395cd2b44004e52a1ef0
jonas <jfehodgson@uamv.edu.ni>
2025-10-16 20:59:11 -0600

    funcion para marcar tarea como realizada

--- Gestiondetareas.cs
+++ Gestiondetareas.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
 
+namespace Marcartarea
+{
+    internal class funcionRealizarTarea
+    {
+        static void RealizarTarea(Queue<string> tareas)
+        {
+            Console.Clear();
+            Console.WriteLine("** TAREA REALIZADA **");
+            if (tareas.Count > 0)
+            {
+                string tarea_en_proceso = tareas.Dequeue();
+                Console.WriteLine($"Tarea completada: \t{tarea_en_proceso}");
+            }
+            else
+            {
+                Console.WriteLine("No hay tareas por realizar.");
+            }
+                Console.WriteLine("Presione cualuquier tecla para continuar...");
+                Console.ReadKey();
+        }
+    }
+}
